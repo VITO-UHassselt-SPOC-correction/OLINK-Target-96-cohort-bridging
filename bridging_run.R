f7abe10a9a2353d18b139a877393bc1ff5fbe5d8
Dries Heylen <91141212+driesheylen123@users.noreply.github.com>
2024-09-27 15:48:45 +0200

    Update bridging_run.R

--- bridging_run.R
+++ bridging_run.R
@@ -5,11 +5,10 @@ filename2 <- "./data/yourdata_NPX.xlsx"
 #Additional batches can be included completely analogue to filename 1 and filename 2
 
 #load the reference pool that includes all proteins that OLINK measures with target 96.
-filename3 <- "./data/reference pool/pooleffect_prep_correct.xlsx"
 filename4 <- "./data/output/pooleffect_scriptinput.xlsx"
 
 #additional filenames should be included below
-run_fun(filename1, filename2, filename3)
+run_fun(filename1, filename2)
 
 
 
